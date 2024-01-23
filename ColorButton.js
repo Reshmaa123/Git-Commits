96f995b9235ae53532a40fa7ce1a119821ef98ad
Reshma <reddyreshma85@gmai.com>
2024-01-23 11:31:35 -0500

    first commit

---
+++ ColorButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import $ from 'jquery'
+import { useState } from 'react';
+import { Icon } from 'semantic-ui-react'
+const Mahesh = () => {
+        
+    const[borderColor,setColor]=useState("black");
+   const changecolor =(color)=>{
+    setColor(color)
+   }
+const Yellow=()=>{
+    setColor("yellow")
+}
+  return (
+    <div>
+        
+      <p id="img1" style={{ borderColor: "black", borderWidth: "2px", borderStyle: "solid", backgroundColor:borderColor}} >Hgfello</p>
+< Icon className='circle outline' style={{backgroundColor:borderColor}}/>
+<button  onClick={()=>changecolor("green")} id="img1">green</button>
+<button onClick={Yellow}>yellow</button>
+<button  onClick={()=>changecolor("blue")} id="img1">blue</button>
+<button  onClick={()=>changecolor("pink")} id="img1">pink</button>
+    </div>
+  )
+}
+
+export default Mahesh
